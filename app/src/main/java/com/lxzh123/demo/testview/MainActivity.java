cf6dab46f7ec9b6922941fd8bb830ff62ba560b4
ljf1239848066 <1239848066@qq.com>
2018-07-22 00:46:50 +0800

    change MainActivity to ListActivity

--- app/src/main/java/com/lxzh123/demo/testview/MainActivity.java
+++ app/src/main/java/com/lxzh123/demo/testview/MainActivity.java
@@ -1,5 +1,12 @@
 package com.lxzh123.demo.testview;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
 import com.lxzh123.demo.activityskip.SkipActivity1;
 import com.lxzh123.demo.apadterview.AdapterViewActivity;
 import com.lxzh123.demo.buttonflash.ButtonFlashActivity;
@@ -18,87 +25,92 @@ import com.lxzh123.demo.viewflipper.ViewFlipperActivity;
 import com.lxzh123.demo.viewpager.ViewPagerActivity;
 import com.lxzh123.demo.wechatbtn.WeChatButtonActivity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
-public class MainActivity extends Activity{
+public class MainActivity extends ListActivity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+        String[] funcStr=new String[]{
+            "ButtonFlash",
+            "AlertDialog",
+            "ViewFlipper",
+            "PendingSkip",
+            "SkipActivity",
+            "AdapterView",
+            "CycleWheelView",
+            "ShapeView",
+            "Seekbar",
+            "RadioButton",
+            "ViewPager",
+            "ProgressAnim",
+            "LinearText",
+            "Rotate1",
+            "Rotate2",
+            "WeChatBtn",
+            "SwitchView"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.main,funcStr);
+        this.setListAdapter(adapter);
+        this.getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                switch (position) {
+                    case 0:
+                        intent.setClass(MainActivity.this, ButtonFlashActivity.class);
+                        break;
+                    case 1:
+                        intent.setClass(MainActivity.this, DialogActivity.class);
+                        break;
+                    case 2:
+                        intent.setClass(MainActivity.this, ViewFlipperActivity.class);
+                        break;
+                    case 3:
+                        intent.setClass(MainActivity.this, PendingSkipActivity1.class);
+                        break;
+                    case 4:
+                        intent.setClass(MainActivity.this, SkipActivity1.class);
+                        break;
+                    case 5:
+                        intent.setClass(MainActivity.this, AdapterViewActivity.class);
+                        break;
+                    case 6:
+                        intent.setClass(MainActivity.this, CycleWheelViewActivity.class);
+                        break;
+                    case 7:
+                        intent.setClass(MainActivity.this, ShapeViewActivity.class);
+                        break;
+                    case 8:
+                        intent.setClass(MainActivity.this, SeekbarActivity.class);
+                        break;
+                    case 9:
+                        intent.setClass(MainActivity.this, RadioButtonActivity.class);
+                        break;
+                    case 10:
+                        intent.setClass(MainActivity.this, ViewPagerActivity.class);
+                        break;
+                    case 11:
+                        intent.setClass(MainActivity.this, ProgressAnimActivity.class);
+                        break;
+                    case 12:
+                        intent.setClass(MainActivity.this, LinearTextActivity.class);
+                        break;
+                    case 13:
+                        intent.setClass(MainActivity.this, Rotate3D1Activity.class);
+                        break;
+                    case 14:
+                        intent.setClass(MainActivity.this, Rotate3D2Activity.class);
+                        break;
+                    case 15:
+                        intent.setClass(MainActivity.this, WeChatButtonActivity.class);
+                        break;
+                    case 16:
+                        intent.setClass(MainActivity.this, SwitchViewActivity.class);
+                        break;
+                    default:
+                        return;
+                }
+                startActivity(intent);
+            }
+        });
 	}
-	//方法修饰符必须为public，private和protected都不行
-	public void OnButtonClick(View v) {
-		Intent intent = new Intent();
-		switch (v.getId()) {
-		case R.id.btnButtonFlash:
-			intent.setClass(MainActivity.this, ButtonFlashActivity.class);
-			break;
-		case R.id.btnDialog:
-			intent.setClass(MainActivity.this, DialogActivity.class);
-			break;
-		case R.id.btnViewFlipper:
-			intent.setClass(MainActivity.this, ViewFlipperActivity.class);
-			break;
-		case R.id.btnPendingSkip:
-			intent.setClass(MainActivity.this, PendingSkipActivity1.class);
-			break;
-		case R.id.btnSkipActivity:
-			intent.setClass(MainActivity.this, SkipActivity1.class);
-			break;
-		case R.id.btnAdapterView:
-			intent.setClass(MainActivity.this, AdapterViewActivity.class);
-			break;
-		case R.id.btnCycleWheelView:
-			intent.setClass(MainActivity.this, CycleWheelViewActivity.class);
-			break;
-		case R.id.btnShapeView:
-			intent.setClass(MainActivity.this, ShapeViewActivity.class);
-			break;
-		case R.id.btnSeekbar:
-			intent.setClass(MainActivity.this, SeekbarActivity.class);
-			break;
-		case R.id.btnRadioButton:
-			intent.setClass(MainActivity.this, RadioButtonActivity.class);
-			break;
-		case R.id.btnViewPager:
-			intent.setClass(MainActivity.this, ViewPagerActivity.class);
-			break;
-		case R.id.btnProgressAnim:
-			intent.setClass(MainActivity.this, ProgressAnimActivity.class);
-			break;
-		case R.id.btnLinearText:
-			intent.setClass(MainActivity.this, LinearTextActivity.class);
-			break;
-		case R.id.btnRotate1:
-			intent.setClass(MainActivity.this, Rotate3D1Activity.class);
-			break;
-		case R.id.btnRotate2:
-			intent.setClass(MainActivity.this, Rotate3D2Activity.class);
-			break;
-		case R.id.btnWeChatBtn:
-			intent.setClass(MainActivity.this, WeChatButtonActivity.class);
-			break;
-		case R.id.btnSwitchView:
-			intent.setClass(MainActivity.this, SwitchViewActivity.class);
-			break;
-//		case R.id.btnRotate1:
-//			intent.setClass(MainActivity.this, Rotate3D1Activity.class);
-//			break;
-//		case R.id.btnRotate2:
-//			intent.setClass(MainActivity.this, Rotate3D2Activity.class);
-//			break;
-//		case R.id.btnWeChatBtn:
-//			intent.setClass(MainActivity.this, WeChatButtonActivity.class);
-//			break;
-		default:
-			return;
-		}
-		startActivity(intent);
-	}
-	
 }
